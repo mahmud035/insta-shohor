37ac8535a5090b8a468c0be6ca516d6ab5a87d45
mahmud035 <mahamudulhasan4148@gmail.com>
2022-03-10 22:37:11 +0600

    user comment issue fixed

--- js/app.js
+++ js/app.js
@@ -29,8 +29,8 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-  console.log(text);
-  console.log(text.length);
+  // console.log(text);
+  // console.log(text.length);
   return text.length < 30
     ? text
     : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
@@ -58,7 +58,7 @@ const switchTab = (id) => {
 
 const createPost = (post) => {
   const image = post.image;
-  // console.log(post.userImage);
+  // console.log(post);
   const div = document.createElement('article');
   div.classList.add('post');
   div.innerHTML = `
@@ -131,9 +131,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post?.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post?.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
